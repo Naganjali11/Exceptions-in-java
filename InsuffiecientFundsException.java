04b7b8dbf56be3196f69ce0690247a17ec849bb7
Naganjali11 <naganjali425@gmail.com>
2025-10-15 20:07:00 +0530

    javafullstackprograms

---
+++ InsuffiecientFundsException.java
@@ -0,0 +1,32 @@
+package com.codegnan.exceptions;
+
+@SuppressWarnings("serial")
+public class InsuffiecientFundsException extends Exception {
+
+	public InsuffiecientFundsException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsuffiecientFundsException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsuffiecientFundsException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsuffiecientFundsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsuffiecientFundsException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
