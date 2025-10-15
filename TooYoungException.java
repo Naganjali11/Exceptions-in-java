85338c956f7ef5829015dfac8de77543395b606e
Naganjali11 <naganjali425@gmail.com>
2025-10-15 20:08:34 +0530

    javafullstackprograms

---
+++ TooYoungException.java
@@ -0,0 +1,32 @@
+package com.codegnan.exceptions;
+
+@SuppressWarnings("serial")
+public class TooYoungException extends Exception{
+
+	public TooYoungException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TooYoungException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TooYoungException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TooYoungException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TooYoungException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
