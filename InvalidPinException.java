b7107dea94ca72d3a51b921b8b77046e60f88836
Naganjali11 <naganjali425@gmail.com>
2025-10-15 20:07:38 +0530

    javafullstackprograms

---
+++ InvalidPinException.java
@@ -0,0 +1,31 @@
+package com.codegnan.exceptions;
+
+@SuppressWarnings("serial")
+public class InvalidPinException extends Exception {
+
+	public InvalidPinException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPinException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPinException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPinException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InvalidPinException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
