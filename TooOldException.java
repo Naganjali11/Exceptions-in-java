9343c33ae265cafd65905f15e9b4db197485fc20
Naganjali11 <naganjali425@gmail.com>
2025-10-15 20:08:16 +0530

    javafullstackprograms

---
+++ TooOldException.java
@@ -0,0 +1,33 @@
+package com.codegnan.exceptions;
+
+@SuppressWarnings("serial")
+public class TooOldException extends Exception {
+
+	public TooOldException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TooOldException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TooOldException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TooOldException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public TooOldException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
